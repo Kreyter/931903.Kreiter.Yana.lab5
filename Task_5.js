1ac6929006454e76ed22b80da19c133137c0f4a8
Kreyter <90960153+Kreyter@users.noreply.github.com>
2021-10-30 22:33:33 +0700

    Add files via upload

---
+++ Task_5.js
@@ -0,0 +1,26 @@
+﻿bt1.onclick = function () {
+    news1.style.display = "block"
+    back1.style.display = "block"
+}
+
+bt2.onclick = function () {
+    news2.style.display = "block"
+    back2.style.display = "block"
+}
+
+bt3.onclick = function () {
+    news3.style.display = "block"
+    back3.style.display = "block"
+}
+news1.onclick = function () {
+    news1.style.display = "none"
+    back1.style.display = "none"
+}
+news2.onclick = function () {
+    news2.style.display = "none"
+    back2.style.display = "none"
+}
+news3.onclick = function () {
+    news3.style.display = "none"
+    back3.style.display = "none"
+}
